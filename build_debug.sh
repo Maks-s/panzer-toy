0a2c0268d66f8582aee135e34f260c6617f211be
Maks-s <karlmaks-s@protonmail.com>
2020-02-08 09:14:58 +0100

    Remove -Werror from debug build

--- build_debug.sh
+++ build_debug.sh
@@ -2,6 +2,6 @@
 
 # I know it sucks, I just have no shame
 
-g++ -ggdb src/*.cpp -msse2 -Wall -Wextra -Wno-deprecated-copy -Werror -o bin/panzerToy.exe -Iinclude -Llib/x64 -Llib -lgl3w -lglfw3 -lgdi32 -lassimp
+g++ -ggdb src/*.cpp -msse2 -Wall -Wextra -Wno-deprecated-copy -o bin/panzerToy.exe -Iinclude -Llib/x64 -Llib -lgl3w -lglfw3 -lgdi32 -lassimp
 
 # This N.E.E.D.S. to be done in CMake instead of a makeshift thingy like this
