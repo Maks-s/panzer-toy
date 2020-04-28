73c77aa0dc9b3baef4702a0c5d7d54c5130828e8
Maks-s <karlmaks-s@protonmail.com>
2020-04-28 20:34:03 +0200

    Make relative paths relative to executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
+#include <filesystem>
+
+#ifdef _WIN32
+	#define VC_EXTRALEAN
+	#define WIN32_LEAN_AND_MEAN
+	#include <Windows.h>
+#else
+	#include <system_error>
+#endif
+
 #include "game.hpp"
 
 int main() {
+	// cd to the executable directory
+	std::filesystem::path executable_path;
+
+	#ifdef _WIN32
+		std::vector<wchar_t> buf;
+		DWORD length;
+
+		do {
+			buf.resize(buf.size() + MAX_PATH);
+			length = GetModuleFileNameW(nullptr, buf.data(), buf.size());
+		} while (GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+
+		buf.resize(length);
+		executable_path = std::wstring(buf.begin(), buf.end());
+	#else
+		// UNTESTED
+		std::error_code code;
+		executable_path = std::filesystem::read_symlink("/proc/self/exe", code);
+
+		if (code.value() != 0) {
+			// Oh shit we don't know our path. Better keep the current one then
+			executable_path.clear();
+		}
+	#endif
+
+	if (!executable_path.empty()) {
+		std::filesystem::current_path(executable_path.parent_path());
+	}
+
 	Game game;
 	game.run();
 	return 0;
